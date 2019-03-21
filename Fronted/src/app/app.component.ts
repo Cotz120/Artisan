e03a169a0530e34483be0348d28f51e46f1ed581
MarcoMulato <marco.antonio.mulato@gmail.com>
2019-03-21 11:40:05 -0600

    Angular

---
+++ Fronted/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Artisan2';
+  cosa(){
+    console.log("Hizo el hola.")
+  }
+}
